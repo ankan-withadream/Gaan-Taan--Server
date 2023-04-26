d81a9078d6e739868821155a5767646a11237693
ankan-withadream <ankanwithadream@gmail.com>
2023-04-26 19:24:07 +0530

    Initial audio uploading to mega.nz working

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const parser = require('body-parser');
 
 
 const app = express();
-// const mega_storage = new mega.Storage({email: "cocgreenranger@gmail.com", password: "Iamankan777",});
+const mega_storage = new mega.Storage({email: "cocgreenranger@gmail.com", password: "Iamankan777",});
 
 const corsOptions ={
     origin:'*', 
@@ -23,8 +23,9 @@ const corsOptions ={
 
 
  app.post('/upload-audio', async (req, res) => {
-  console.log("Body received", req.files, req.body, req.body.audioFile, req.form);
-  // // const { audioUrl, filename } = req.body;
+  console.log("Body received", req.files, req.body, req.data, req.body.audioFile, req.form);
+  // console.log("Mega Storage", mega_storage);
+  // const { audioUrl, filename } = req.body;
   // let filename = req.body.filename;
   //   let audioUrl = req.body.audioUrl;
 
@@ -32,7 +33,10 @@ const corsOptions ={
   // const audioStream = request.get({uri: audioUrl});
   
   // // create a Writeable stream for uploading the audio file to MEGA
-  // const megaFileStream = mega_storage.upload({ name: filename });
+  const megaFileStream = mega_storage.upload({ 
+    name: "new.mp3",
+    allowUploadBuffering: true,
+});
 
   // // pipe the audio stream to the MEGA file stream
   // audioStream.pipe(megaFileStream);
@@ -46,16 +50,25 @@ const corsOptions ={
   // megaFileStream.on('finish', () => {
   //   res.status(200).send('File uploaded to MEGA');
   // });
-  const fileStream = fs.createWriteStream('audio_file5.mp3', {flags: 'a'});
 
-  req.on('data', chunk => {
-    fileStream.write(chunk);
-  });
+  req.pipe(megaFileStream);
+  megaFileStream.on('complete', (file) => {
+    console.log('The file was uploaded!', file)
+  })
+  megaFileStream.on('error', (error) => {
+    console.log('Error uploading file!', file)
+  })
 
-  req.on('end', () => {
-    fileStream.end();
-    res.status(200).send('File uploaded successfully');
-  });
+  // const fileStream = fs.createWriteStream('audio_file5.mp3', {flags: 'a'});
+
+  // req.on('data', chunk => {
+  //   fileStream.write(chunk);
+  // });
+
+  // req.on('end', () => {
+  //   fileStream.end();
+  //   res.status(200).send('File uploaded successfully');
+  // });
 });
 
 
