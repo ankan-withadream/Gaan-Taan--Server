718d76b7ef63e8f07975bd7251219e5b251304bb
Ankan <ankanwithadream@gmail.com>
2023-10-20 06:11:57 +0000

    figuring out all the DB CRUDs

--- src/features/music/music.controller.js
+++ src/features/music/music.controller.js
@@ -1,47 +1,51 @@
 import { File } from "megajs";
+import mongoose from "mongoose";
+import { Music } from "./music.model.js";
 
 
 const streamMusic = (async (req, res) => {
-  let musicId = req.params.musicId;
-  console.log("music id: ", musicId);
-  let url = "https://mega.nz/file/c3pnFJLS#otCIH5YLhnNwSB0sp1YYKr9zNMc4MAE7G0Wj_EzR4mQ";
-  const file = File.fromURL(url);
-  console.log("File", file);
-  await file.loadAttributes();
-  console.log("GGGGGG",);
 
-  console.log("File Loadatri", file.name);
-  // const start = fs.statSync(file.name).size
-  const downloadStream = file.download();
+    let music_id = 1;
+    console.log("done")
+    let newMusic = new Music({
+        music_id: 3,
+        music_name: "Song name",
+        music_author: "author authort",
+        music_url: "any.url//asdklfj",
+        music_image: "test"
+    });
+    await newMusic.save();
+    console.log("done 2")
 
-  // set the response headers for audio file
-  res.setHeader('Content-Type', 'audio/mpeg');
-  res.setHeader('Content-Disposition', 'inline');
+    let url = "https://mega.nz/file/c3pnFJLS#otCIH5YLhnNwSB0sp1YYKr9zNMc4MAE7G0Wj_EzR4mQ";
+    const file = File.fromURL(url);
+    console.log("File", file);
+    await file.loadAttributes();
+    console.log("GGGGGG",);
 
-  // use request module to stream the audio file directly to the response object
-  // request.get(audioUrl).pipe(res);
-  downloadStream.pipe(res);
+    console.log("File Loadatri", file.name);
+    // const start = fs.statSync(file.name).size
+    const downloadStream = file.download();
+
+    // set the response headers for audio file
+    res.setHeader('Content-Type', 'audio/mpeg');
+    res.setHeader('Content-Disposition', 'inline');
+
+    // use request module to stream the audio file directly to the response object
+    // request.get(audioUrl).pipe(res);
+    downloadStream.pipe(res)
 });
 
 const uploadMusic = (async (req, res) => {
-  console.log("Body received", req.files, req.body, req.data, req.body.audioFile, req.form);
-  // create a Writeable stream for uploading the audio file to MEGA
-  const megaFileStream = mega_storage.upload({
-    name: "sample.mp3",
-    allowUploadBuffering: true,
-  });
+    let newMusic = new Music({
+        music_id: 3,
+        music_name: "Song name",
+        music_author: "author authort",
+        music_url: "any.url//asdklfj",
+        music_image: "test"
+    });
+    await newMusic.save();
 
-  req.pipe(megaFileStream);
-  megaFileStream.on('complete', async (file) => {
-    console.log('The file was uploaded!',)
-    const fileUrl = await file.link();
-    console.log("File Url: ", fileUrl);
-    res.send(fileUrl)
-  })
-  megaFileStream.on('error', (error) => {
-    console.log('Error uploading file!', file);
-    res.send(error)
-  })
 })
 
 export { streamMusic, uploadMusic };
--- src/features/music/music.router.js
+++ src/features/music/music.router.js
@@ -1,67 +1,15 @@
 import { Router } from "express";
 import { File } from "megajs";
-import { streamMusic } from "./music.controller.js";
+import { streamMusic, uploadMusic } from "./music.controller.js";
 
 const musicRouter = Router();
 
 musicRouter.get('/stream/:musicId', streamMusic);
 
 
-musicRouter.post('/upload-audio', async (req, res) => {
-    console.log("Body received", req.files, req.body, req.data, req.body.audioFile, req.form);
-    // console.log("Mega Storage", mega_storage);
-    // const { audioUrl, filename } = req.body;
-    // let filename = req.body.filename;
-    //   let audioUrl = req.body.audioUrl;
-  
-    // // create a Readable stream for the audio file from the given URL
-    // const audioStream = request.get({uri: audioUrl});
-    
-    // // create a Writeable stream for uploading the audio file to MEGA
-    const megaFileStream = mega_storage.upload({ 
-      name: "sample.mp3",
-      allowUploadBuffering: true,
-  });
-  
-    // // pipe the audio stream to the MEGA file stream
-    // audioStream.pipe(megaFileStream);
-  
-    // // handle stream events and send response when done
-    // megaFileStream.on('error', (err) => {
-    //   console.error(err);
-    //   res.status(500).send('Error uploading file to MEGA');
-    // });
-  
-    // megaFileStream.on('finish', () => {
-    //   res.status(200).send('File uploaded to MEGA');
-    // });
-  
-    req.pipe(megaFileStream);
-    megaFileStream.on('complete', async (file) => {
-      console.log('The file was uploaded!', )
-      const fileUrl = await file.link();
-      console.log("File Url: ", fileUrl);
-  
-  
-  
-    })
-    megaFileStream.on('error', (error) => {
-      console.log('Error uploading file!', file)
-    })
-  
-    // const fileStream = fs.createWriteStream('audio_file5.mp3', {flags: 'a'});
-  
-    // req.on('data', chunk => {
-    //   fileStream.write(chunk);
-    // });
-  
-    // req.on('end', () => {
-    //   fileStream.end();
-    //   res.status(200).send('File uploaded successfully');
-    // });
-  });
+musicRouter.post('/upload-audio', uploadMusic);
 
 
 
 
-export {musicRouter};
+export { musicRouter };
