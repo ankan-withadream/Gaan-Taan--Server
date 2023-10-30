59c1b5d0383fd17af8250feb313872ee7dc546de
Ankan <ankanwithadream@gmail.com>
2023-10-30 15:01:33 +0000

    Music upload working properly

--- src/features/music/music.controller.js
+++ src/features/music/music.controller.js
@@ -6,52 +6,56 @@ import { mega_storage } from "../../services/server.js";
 
 const streamMusic = (async (req, res) => {
 
-    let music = await Music.findOne({music_id: req.params.musicId});
-    // let url = "https://mega.nz/file/c3pnFJLS#otCIH5YLhnNwSB0sp1YYKr9zNMc4MAE7G0Wj_EzR4mQ";
-    let url = music["music_url"];
-    const file = File.fromURL(url);
-    await file.loadAttributes();
-    console.log("File name:", file.name);
-    // const start = fs.statSync(file.name).size
-    const downloadStream = file.download();
+  let music = await Music.findOne({ music_id: req.params.musicId });
+  // let url = "https://mega.nz/file/c3pnFJLS#otCIH5YLhnNwSB0sp1YYKr9zNMc4MAE7G0Wj_EzR4mQ";
+  let url = music["music_url"];
+  const file = File.fromURL(url);
+  await file.loadAttributes();
+  console.log("File name:", file.name);
+  // const start = fs.statSync(file.name).size
+  const downloadStream = file.download();
 
-    // set the response headers for audio file
-    res.setHeader('Content-Type', 'audio/mpeg');
-    res.setHeader('Content-Disposition', 'inline');
+  // set the response headers for audio file
+  res.setHeader('Content-Type', 'audio/mpeg');
+  res.setHeader('Content-Disposition', 'inline');
 
-    // use request module to stream the audio file directly to the response object
-    // request.get(audioUrl).pipe(res);
-    downloadStream.pipe(res)
+  // use request module to stream the audio file directly to the response object
+  // request.get(audioUrl).pipe(res);
+  downloadStream.pipe(res)
 });
 
 const uploadMusic = (async (req, res) => {
-    let newMusic = new Music({
-        music_id: 3,
-        music_name: "Song name",
-        music_author: "author authort",
-        music_url: "any.url//asdklfj",
-        music_image: "test"
-    });
-    await newMusic.save();
 
-    console.log("Body received", req.files, req.body, req.data, req.body.audioFile, req.form);
-    // create a Writeable stream for uploading the audio file to MEGA
-    const megaFileStream = mega_storage.upload({
-      name: "sample.mp3",
-      allowUploadBuffering: true,
+  console.log("Body received", req.files, req.body, req.data, req.body.audioFile, req.form);
+  // create a Writeable stream for uploading the audio file to MEGA
+  const megaFileStream = mega_storage.upload({
+    name: req.body.name,
+    allowUploadBuffering: true,
+  });
+
+  req.pipe(megaFileStream);
+  megaFileStream.on('complete', async (file) => {
+    console.log('The file was uploaded!',)
+    const fileUrl = await file.link();
+    console.log("File Url: ", fileUrl);
+
+
+    // The mongodb insertion part
+    await Music.create({
+      music_id: new Date().valueOf(),
+      music_name: req.body.name,
+      music_author: req.body.author,
+      music_url: fileUrl,
+      music_image: "test"
     });
-  
-    req.pipe(megaFileStream);
-    megaFileStream.on('complete', async (file) => {
-      console.log('The file was uploaded!',)
-      const fileUrl = await file.link();
-      console.log("File Url: ", fileUrl);
-      res.send(fileUrl)
-    })
-    megaFileStream.on('error', (error) => {
-      console.log('Error uploading file!', file);
-      res.send(error)
-    })
+
+    res.send(fileUrl)
+  })
+  megaFileStream.on('error', (error) => {
+    console.log('Error uploading file!', file);
+    res.send(error)
+  })
+
 })
 
 export { streamMusic, uploadMusic };
